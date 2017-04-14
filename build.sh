41d2af28d5eb28351677ea46ed38040889897bc6
Silvio Simunic <silvio.simunic@gmail.com>
2017-04-14 03:11:58 +0200

    Fix GOARCH in build script

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Building for Linux"
-GOOS="linux" GOOS="linux" go build -o jsonscraper_linux
+GOARCH="amd64" GOOS="linux" go build -o jsonscraper_linux
 
 echo "Building for Darwin"
-GOOS="linux" GOOS="darwin" go build -o jsonscraper_darwin
+GOARCH="amd64" GOOS="darwin" go build -o jsonscraper_darwin
 
 echo "Building for Windows"
-GOOS="linux" GOOS="windows" go build -o jsonscraper_windows.exe
+GOARCH="amd64" GOOS="windows" go build -o jsonscraper_windows.exe
